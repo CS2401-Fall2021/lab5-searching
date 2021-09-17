bab6b7e98b20a4758ac419649306e9cebedde921
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-17 10:05:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     System.out.println("Expected linear steps: 25");
     System.out.println("Actual linear steps: " +BugFinder.findViaLinearSearch(n,targetIndex));
-    System.out.println("Expected binary steps: 5");
+    System.out.println("Expected binary steps: 6");
     System.out.println("Actual binary steps: " +BugFinder.findViaBinarySearch(n,targetIndex));
     System.out.println("Expected isBinaryBetter: true");
     System.out.println("Actual isBinaryBetter : " +BugFinder.isBinarySearchBetter(n,targetIndex));
