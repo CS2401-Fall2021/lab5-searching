c6dfae877555090ac07d9e5331679ca94c57eac1
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-17 09:13:16 -0600

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Used for running automatic tests. DO NOT EDIT
+public class Test {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String line = scanner.nextLine();
+    String[] inputs=line.split(",");
+
+    int n =Integer.parseInt(inputs[1]);
+    int i =Integer.parseInt(inputs[2]);
+    if(inputs[0].equals("ls")){
+      System.out.println("l:"+BugFinder.findViaLinearSearch(n,i));
+    }
+    if(inputs[0].equals("bs")){
+      System.out.println("b:"+BugFinder.findViaBinarySearch(n,i));
+    }
+    if(inputs[0].equals("c")){
+      System.out.println("c:"+BugFinder.isBinarySearchBetter(n,i));
+    }
+
+  }
+
+}
