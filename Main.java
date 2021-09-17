89deb8c98c2d392188aac90deebf9826642dc774
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-17 09:15:08 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int n=100;
+    int targetIndex =25;
+
+    System.out.println("Expected linear steps: 25");
+    System.out.println("Actual linear steps: " +BugFinder.findViaLinearSearch(n,targetIndex));
+    System.out.println("Expected binary steps: 5");
+    System.out.println("Actual binary steps: " +BugFinder.findViaBinarySearch(n,targetIndex));
+    System.out.println("Expected isBinaryBetter: true");
+    System.out.println("Actual isBinaryBetter : " +BugFinder.isBinarySearchBetter(n,targetIndex));
+  }
+}
