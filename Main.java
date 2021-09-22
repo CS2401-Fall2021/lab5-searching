a02f63af7a8dd203a93fd9b1e2f4fc27818086d9
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-22 13:14:11 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,26 @@ public class Main {
   public static void main(String[] args) {
     int n=100;
     int targetIndex =25;
-
+    boolean[] emulation= emulateCodeSubmissions(n,targetIndex);
     System.out.println("Expected linear steps: 25");
-    System.out.println("Actual linear steps: " +BugFinder.findViaLinearSearch(n,targetIndex));
-    System.out.println("Expected binary steps: 6");
-    System.out.println("Actual binary steps: " +BugFinder.findViaBinarySearch(n,targetIndex));
+    System.out.println("Actual linear steps: " +BugFinder.findViaLinearSearch(emulateCodeSubmissions(n,targetIndex)));
+    System.out.println("Expected binary steps: 5");
+    System.out.println("Actual binary steps: " +BugFinder.findViaBinarySearch(emulateCodeSubmissions(n,targetIndex)));
     System.out.println("Expected isBinaryBetter: true");
-    System.out.println("Actual isBinaryBetter : " +BugFinder.isBinarySearchBetter(n,targetIndex));
+    System.out.println("Actual isBinaryBetter : " +BugFinder.isBinarySearchBetter(emulateCodeSubmissions(n,targetIndex)));
   }
+
+  public static boolean[] emulateCodeSubmissions(int numberOfChanges, int indexOfFailure) {
+
+    boolean[] result = new boolean[numberOfChanges];
+    for (int i = 0; i < numberOfChanges; i++) {
+      if (i < indexOfFailure) {
+        result[i] = true;
+      } else {
+        result[i] = false;
+      }
+    }
+    return result;
+  }
+
 }
