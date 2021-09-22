36f88b8967cb618198caef8d139b920af64718a5
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-22 15:18:34 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ public class Main {
     boolean[] emulation= emulateCodeSubmissions(n,targetIndex);
     System.out.println("Expected linear steps: 25");
     System.out.println("Actual linear steps: " +BugFinder.findViaLinearSearch(0,emulateCodeSubmissions(n,targetIndex)));
-    System.out.println("Expected binary steps: 5");
+    System.out.println("Expected binary steps: 2");
+    // HINT: your midpoints should be 49, then 24. index 24 is true, and it looks forward to index 25, which is our first false.
     System.out.println("Actual binary steps: " +BugFinder.findViaBinarySearch(0,n-1,emulateCodeSubmissions(n,targetIndex)));
     System.out.println("Expected isBinaryBetter: true");
     System.out.println("Actual isBinaryBetter : " +BugFinder.isBinarySearchBetter(emulateCodeSubmissions(n,targetIndex)));
