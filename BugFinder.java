4880201292ea7b268d62608a6e0afdb45537ac19
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-22 14:47:08 -0600

    Update BugFinder.java

--- BugFinder.java
+++ BugFinder.java
@@ -4,12 +4,12 @@ public class BugFinder {
   }
 
 
-  public static int findViaLinearSearch(boolean[] changes) {
+  public static int findViaLinearSearch(int startIndex, boolean[] changes) {
 
     return 0;
   }
 
-  public static int findViaBinarySearch(boolean[] changes) {
+  public static int findViaBinarySearch(int startIndex, int endIndex, boolean[] changes) {
 
      return 0;
   }
