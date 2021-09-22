61ce16f531772d2ac045b8f1b14749ee02b8a953
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-22 13:13:50 -0600

    Update BugFinder.java

--- BugFinder.java
+++ BugFinder.java
@@ -3,30 +3,18 @@ public class BugFinder {
   public static void BugFinder() {
   }
 
-  public static boolean[] emulateCodeSubmissions(int numberOfChanges, int indexOfFailure) {
 
-    boolean[] result = new boolean[numberOfChanges];
-    for (int i = 0; i < numberOfChanges; i++) {
-      if (i < indexOfFailure) {
-        result[i] = true;
-      } else {
-        result[i] = false;
-      }
-    }
-    return result;
-  }
+  public static int findViaLinearSearch(boolean[] changes) {
 
-  public static int findViaLinearSearch(int numberOfChanges, int indexOfFailure) {
-    boolean[] changes = emulateCodeSubmissions(numberOfChanges, indexOfFailure);
     return 0;
   }
 
-  public static int findViaBinarySearch(int numberOfChanges, int indexOfFailure) {
-    boolean[] changes = emulateCodeSubmissions(numberOfChanges, indexOfFailure);
-    return 0;
+  public static int findViaBinarySearch(boolean[] changes) {
+
+     return 0;
   }
 
-  public static boolean isBinarySearchBetter(int numberOfChanges, int indexOfFailure) {
+  public static boolean isBinarySearchBetter(boolean[] changes) {
     return false;
   }
 }
