4ec11c4f3e4532b8ebd28c61eaae39167bb4ac71
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-22 13:15:57 -0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,16 +9,28 @@ public class Test {
 
     int n =Integer.parseInt(inputs[1]);
     int i =Integer.parseInt(inputs[2]);
-    if(inputs[0].equals("l")){
-      System.out.println("l:"+BugFinder.findViaLinearSearch(n,i));
+    if(inputs[0].equals("ls")){
+      System.out.println("l:"+BugFinder.findViaLinearSearch(emulateCodeSubmissions(n,i)));
     }
-    if(inputs[0].equals("b")){
-      System.out.println("b:"+BugFinder.findViaBinarySearch(n,i));
+    if(inputs[0].equals("bs")){
+      System.out.println("b:"+BugFinder.findViaBinarySearch(emulateCodeSubmissions(n,i)));
     }
     if(inputs[0].equals("c")){
-      System.out.println("c:"+BugFinder.isBinarySearchBetter(n,i));
+      System.out.println("c:"+BugFinder.isBinarySearchBetter(emulateCodeSubmissions(n,i)));
     }
 
+  }
+  public static boolean[] emulateCodeSubmissions(int numberOfChanges, int indexOfFailure) {
+
+    boolean[] result = new boolean[numberOfChanges];
+    for (int i = 0; i < numberOfChanges; i++) {
+      if (i < indexOfFailure) {
+        result[i] = true;
+      } else {
+        result[i] = false;
+      }
+    }
+    return result;
   }
 
 }
